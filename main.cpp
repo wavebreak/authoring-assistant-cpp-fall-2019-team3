2c3def39af42079ea2076b8c799d5c7fc6e492f2
T20152967 <michael.keegan@tulsacc.edu>
2019-10-08 10:14:17 -0500

    main and menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  
+//
 //
 //  Created by William Smith on 10/2/19.
 //
@@ -8,9 +8,29 @@
 #include <string>
 using namespace std;
 
-int main() {
+char printMenu(string usrStr) {
+    char menuOp;
 
-   /* Type your code here. */
+    cout << "\nMENU" << endl;
+    cout << "c - Number of non-whitespace characters" << endl;
+    cout << "w - Number of words" << endl;
+    cout << "f - Find text" << endl;
+    cout << "r - Replace all !'s" << endl;
+    cout << "s - Shorten spaces" << endl;
+    cout << "q - Quit" << endl;
+    cout << "\nChoose an option: " << endl;
+
+    cin >> menuOp;
+}
+
+int main() {
+    string userString;
+
+    cout << "Enter a string: ";
+    getline(cin, userString);
+    cout << "You entered: " << userString << endl;
+    printMenu(userString);
+
 
    return 0;
 }
